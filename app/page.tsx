9c3088cfb4aa240cbffe46d8f97c58c80003e033
Crash <1903291+hex22a@users.noreply.github.com>
2025-01-10 15:54:45 +0400

    Add image assets and layout styles for UI enhancement
    Added new image and vector assets, including logos and illustrations, for improved visual presentation. Introduced a responsive container class in CSS to structure UI layout adaptively. These changes enhance the overall design consistency and user experience.

--- app/page.tsx
+++ app/page.tsx
@@ -1,104 +1,87 @@
 import Image from "next/image";
+import layout from "@/app/ui/layout.module.css"
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <h1>
-          BEST TECHWEAR SHOP
-        </h1>
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+        <main>
+            <div className={`${layout.container} block md:grid md:grid-cols-2`}>
+                <div className="flex flex-col justify-center items-start">
+                    <h1>FIND CLOTHES THAT MATCHES YOUR STYLE</h1>
+                    <p>Browse through our diverse range of meticulously crafted garments, designed to bring out your
+                        individuality and cater to your sense of style.</p>
+                    <button>Shop Now</button>
+                    <div>
+                        <div className="inline-block">
+                            <h2>200+</h2>
+                            <p>International Brands</p>
+                        </div>
+                        <div className="inline-block">
+                            <h2>2,000+</h2>
+                            <p>High-Quality Products</p>
+                        </div>
+                        <div className="inline-block">
+                            <h2>30,000+</h2>
+                            <p>Happy Customers</p>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <Image
+                        src="/woman.webp"
+                        alt="woman in clothes"
+                        width={1280}
+                        height={1280}
+                    />
+                </div>
+            </div>
+            <div className="bg-black">
+                <div className={`${layout.container} flex flex-row flex-wrap justify-around items-center min-h-32`}>
+                    <div className="relative min-h-10 min-w-24 md:min-w-36 my-4 md:my-0">
+                        <Image
+                            className="object-contain"
+                            src="/krakatau.svg"
+                            alt="krakatau logo"
+                            fill
+                        />
+                    </div>
+                    <div className="relative min-h-10  min-w-24 md:min-w-36 my-4 md:my-0">
+                        <Image
+                            className="object-contain"
+                            src="/arcteryx.svg"
+                            alt="arcteryx logo"
+                            fill
+                        />
+                    </div>
+                    <div className="relative min-h-10  min-w-24 md:min-w-36 my-4 md:my-0">
+                        <Image
+                            className="object-contain"
+                            src="/acronym.svg"
+                            alt="acronym logo"
+                            fill
+                        />
+                    </div>
+                    <div className="relative min-h-10  min-w-24 md:min-w-36 my-4 md:my-0">
+                        <Image
+                            className="object-contain"
+                            src="/cp-company.svg"
+                            alt="cp company logo"
+                            fill
+                        />
+                    </div>
+                    <div className="relative min-h-10 min-w-24 md:min-w-36 my-4 md:my-0">
+                        <Image
+                            className="object-contain"
+                            src="/the-north-face.svg"
+                            alt="north face logo"
+                            fill
+                        />
+                    </div>
+                </div>
+            </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </main>
+        <footer className="grid grid-cols-12">footer</footer>
+    </>
   );
 }
