9c119697b9a1da2da601e80d4bd290d03a6d1ff3
Crash <1903291+hex22a@users.noreply.github.com>
2025-01-17 15:44:49 +0400

    Integrate reusable `OrderForm` component.
    Replaced inline product order form logic with a reusable `OrderForm` component to improve code modularity. Updated product page layout and added custom styles in global CSS to adjust input spinner behavior. This enhances maintainability and UI consistency across the application.

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,5 +1,4 @@
 import layout from "@/app/ui/layout.module.css"
-import styles from "./page.module.css"
 
 import Header from "@/app/ui/header";
 import Footer from "@/app/ui/footer";
@@ -8,6 +7,7 @@ import Image from "next/image";
 import Stars from "@/app/ui/stars";
 import Price from "@/app/ui/price";
 import Featured from "@/app/ui/featured";
+import OrderForm from "@/app/product/ui/order_form";
 
 const sitePath = [
     {
@@ -42,28 +42,32 @@ export default function ProductPage () {
                     <div className="flex flex-col md:flex-row-reverse justify-between items-center gap-3.5">
                         <div className="relative w-full md:w-[530px] h-[290px] md:h-full bg-gray-300 rounded-xl overflow-hidden">
                             <Image
+                                className="object-cover"
                                 src="/items/napa-anor.webp"
                                 alt="Napapijri Anorak"
                                 fill
                             />
                         </div>
-                        <div className="flex flex-row md:flex-col gap-3.5">
-                            <div className="relative w-1/3 md:w-[152px] h-[106px] md:h-[167px] bg-gray-300 rounded-xl overflow-hidden">
+                        <div className="flex flex-row md:flex-col gap-3.5 items-stretch">
+                            <div className="relative min-w-28 min-h-36 md:w-[152px] md:h-[167px] bg-gray-300 rounded-xl overflow-hidden">
                                 <Image
+                                    className="object-cover"
                                     src="/items/NA4I5F176-ALT1.webp"
                                     alt="Napapijri Anorak"
                                     fill
                                 />
                             </div>
-                            <div className="relative w-[152px] h-[167px] bg-gray-300 rounded-xl overflow-hidden">
+                            <div className="relative min-w-28 min-h-36 md:w-[152px] md:h-[167px] bg-gray-300 rounded-xl overflow-hidden">
                                 <Image
+                                    className="object-cover"
                                     src="/items/NA4I5F176-ALT2.webp"
                                     alt="Napapijri Anorak"
                                     fill
                                 />
                             </div>
-                            <div className="relative w-[152px] h-[167px] bg-gray-300 rounded-xl overflow-hidden">
+                            <div className="relative min-w-28 min-h-36 md:w-[152px] md:h-[167px] bg-gray-300 rounded-xl overflow-hidden">
                                 <Image
+                                    className="object-cover"
                                     src="/items/NA4I5F176-ALT3.webp"
                                     alt="Napapijri Anorak"
                                     fill
@@ -85,76 +89,8 @@ export default function ProductPage () {
                             flag patch, and the iconic front flap pocket complete with our signature Napapijri
                             Geographic graphic.
                         </p>
-                        <hr/>
-                        <form>
-                            <fieldset>
-                                <legend>Select Colors</legend>
-                                <label htmlFor="color-red">
-                                    <input className="hidden peer" type="radio" name="color" id="color-red"
-                                           value="red" aria-checked={true}/>
-                                    <div
-                                        className={`${styles.radio_mark_check} relative inline-block w-9 h-9 bg-red-500 rounded-full`}
-                                        tabIndex={0}></div>
-                                </label>
-                                <label htmlFor="color-blue">
-                                    <input className="hidden peer" type="radio" name="color" id="color-blue"
-                                           value="blue" aria-checked={false}/>
-                                    <div
-                                        className={`${styles.radio_mark_check} relative inline-block w-9 h-9 bg-blue-500 rounded-full`}
-                                        tabIndex={0}></div>
-                                </label>
-                                <label htmlFor="color-black">
-                                    <input className="hidden peer" type="radio" name="color" id="color-black"
-                                           value="black" aria-checked={false}/>
-                                    <div
-                                        className={`${styles.radio_mark_check} relative inline-block w-9 h-9 bg-black rounded-full`}
-                                        tabIndex={0}></div>
-                                </label>
-                            </fieldset>
-                            <hr/>
-                            <fieldset>
-                                <legend>Choose size</legend>
-                                <label htmlFor="size-s">
-                                    <input className="hidden peer" type="radio" name="size" id="size-s"
-                                           value="s" aria-checked={true}/>
-                                    <div
-                                        className="relative inline-block bg-gray-300 rounded-full opacity-60 peer-checked:bg-black peer-checked:text-white peer-checked:opacity-100 md:py-3 md:px-6"
-                                        tabIndex={0}>
-                                        Small
-                                    </div>
-                                </label>
-                                <label htmlFor="size-m">
-                                    <input className="hidden peer" type="radio" name="size" id="size-m"
-                                           value="m" aria-checked={false}/>
-                                    <div
-                                        className="relative inline-block bg-gray-300 rounded-full opacity-60 peer-checked:bg-black peer-checked:text-white peer-checked:opacity-100 md:py-3 md:px-6"
-                                        tabIndex={0}>
-                                        Medium
-                                    </div>
-                                </label>
-                                <label htmlFor="size-l">
-                                    <input className="hidden peer" type="radio" name="size" id="size-l"
-                                           value="l" aria-checked={false}/>
-                                    <div
-                                        className="relative inline-block bg-gray-300 rounded-full opacity-60 peer-checked:bg-black peer-checked:text-white peer-checked:opacity-100 md:py-3 md:px-6"
-                                        tabIndex={0}>
-                                        Large
-                                    </div>
-                                </label>
-                                <label htmlFor="size-xl">
-                                    <input className="hidden peer" type="radio" name="size" id="size-xl"
-                                           value="xl" aria-checked={false}/>
-                                    <div
-                                        className="relative inline-block bg-gray-300 rounded-full opacity-60 peer-checked:bg-black peer-checked:text-white peer-checked:opacity-100 md:py-3 md:px-6"
-                                        tabIndex={0}>
-                                        X-Large
-                                    </div>
-                                </label>
-                            </fieldset>
-                            <hr/>
-                            <input type="number" defaultValue={1} min={1} max={100} />
-                            <input type="submit"/>
-                        </form>
+                        <hr className="my-6"/>
+                        <OrderForm/>
                     </div>
                 </div>
             </main>
