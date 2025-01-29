fde1bcd0e7ba35a50959f7e23d29ce982f8b2167
Crash <1903291+hex22a@users.noreply.github.com>
2025-01-29 14:57:00 +0400

    Adjust alignment of product details in the layout
    Changed alignment from `items-stretch` to `items-center` for better visual consistency in the product details section. This improves the layout's appearance, especially on larger screen sizes.

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -49,7 +49,7 @@ export default async function ProductPage (props: {params: Promise<{id: string}>
                     <Breadcrumbs sitePath={sitePath}/>
                 </div>
                 <main className="grid grid-cols-1 md:grid-cols-2 md:gap-x-5">
-                    <div className="flex flex-col md:flex-row-reverse justify-between items-stretch gap-3.5">
+                    <div className="flex flex-col md:flex-row-reverse justify-between items-center gap-3.5">
                         <div
                             className="relative w-full md:w-[530px] h-[290px] md:h-full bg-gray-300 rounded-xl overflow-hidden">
                             {photo_url &&
