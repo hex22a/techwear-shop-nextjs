311e1cc2f1a2428b53136c2f0f191e13042c2906
Crash <1903291+hex22a@users.noreply.github.com>
2025-12-19 00:25:01 +0400

    migration done

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -8,7 +8,7 @@ import { ORIGIN, RP_ID, RP_NAME, USER_VERIFICATION_MODE } from '@/app/lib/consta
 
 export const STRIPE_CONFIG: Stripe.StripeConfig = {
   // https://github.com/stripe/stripe-node#configuration
-  apiVersion: '2025-08-27.basil',
+  apiVersion: '2025-11-17.clover',
   appInfo: {
     name: 'techwear-shop-demo',
     url: 'https://techwear-shop-nextjs.vercel.app/',
